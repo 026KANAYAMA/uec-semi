de92e92c90863f7761b47ceb3622f517c3f130ba
kanayama <103913975+026KANAYAMA@users.noreply.github.com>
2025-05-04 19:45:40 +0900

    Initial Commmit

---
+++ 6_1_gan_ddp.py
@@ -0,0 +1,170 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+import time
+import random
+import glob
+
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.distributed as dist
+from torch.multiprocessing.spawn import spawn
+import torchvision.datasets as dst
+from torchvision.io import read_image
+from torchvision.transforms import v2
+from torch.utils.data import DataLoader, DistributedSampler
+
+def main(rank, world_size):
+    # 環境変数，プロセスグループ初期化
+    os.environ['MASTER_ADDR'] = '127.0.0.1' # ランデブーサーバー（プロセス間を仲介）
+    os.environ['MASTER_PORT'] = '29500'
+    dist.init_process_group(
+        backend='nccl',
+        world_size=world_size,
+        rank=rank
+    )
+
+    # seed
+    seed = 0
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    # device
+    torch.cuda.set_device(rank)
+    device = torch.device(f'cuda:{rank}')
+
+    # settings
+    batch_size = 256 # 1024
+
+    # data
+    dataroot = "/export/data/dataset/mnist_10k"
+
+    dataset = dst.ImageFolder( #ImageFolderはGrayでもRGBで読み込む
+        root=dataroot,
+        transform=v2.Compose([
+            v2.Grayscale(num_output_channels=1),
+            v2.ToImage(),
+            v2.ToDtype(torch.float, scale=True),
+            v2.Normalize([0.5], [0.5])
+        ]),
+    )
+
+    sampler = DistributedSampler(
+        dataset,
+        num_replicas=world_size,
+        rank=rank,
+        shuffle=True
+    )
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=False, # 代わりにepoch内で設定
+        sampler=sampler,
+        num_workers=1,
+        pin_memory=True
+    )
+
+    class Generator(nn.Module): # nn.Moduleは継承しなくてもいいけど一応
+        def __init__(self, z_dim=100, img_dim=28*28):
+            super().__init__()
+            self.net = nn.Sequential(
+                nn.Linear(z_dim, 256),
+                nn.ReLU(True),
+                nn.Linear(256, img_dim),
+                nn.Tanh() # range[-1,1]
+            )
+        def forward(self, z):
+            return self.net(z)
+
+    class Discriminator(nn.Module):
+        def __init__(self, img_dim=28*28):
+            super().__init__()
+            self.net = nn.Sequential(
+                nn.Linear(img_dim, 256),
+                nn.LeakyReLU(0.2, True), # Dが早く飽和して学習しなくなるのを防ぐ
+                nn.Linear(256, 1),
+                nn.Sigmoid()
+            )
+        def forward(self, x):
+            return self.net(x)
+        
+    # exp
+    G = nn.parallel.DistributedDataParallel(
+        Generator().to(device),
+        device_ids=[rank],
+        output_device=rank,
+        find_unused_parameters=False
+    )
+    D = nn.parallel.DistributedDataParallel(
+        Discriminator().to(device),
+        device_ids=[rank],
+        output_device=rank,
+        find_unused_parameters=False
+    )
+
+    opt_G = torch.optim.Adam(G.parameters(), lr=2e-4)
+    opt_D = torch.optim.Adam(D.parameters(), lr=2e-4)
+
+    criterion = nn.BCELoss()
+
+
+    # learning
+    epochs = 100
+    z_dim = 100
+    for epoch in range(epochs):
+        sampler.set_epoch(epoch)
+        total_loss_D = 0
+        total_loss_G = 0
+        for real_imgs, _ in dataloader:
+            real = real_imgs.view(real_imgs.size(0), -1).to(device) #[b,img_dim]
+
+            # 1.Discriminator更新
+            D.zero_grad()
+            real_labels = torch.ones(real.size(0),1, device=device) #本物ラベル
+            pred_real = D(real)
+            loss_real = criterion(pred_real, real_labels)
+
+            # 偽物生成
+            z = torch.randn(real.size(0), z_dim, device=device)
+            fake = G(z).detach()
+            fake_labels = torch.zeros(real.size(0),1, device=device)
+            pred_fake = D(fake)
+            loss_fake = criterion(pred_fake, fake_labels)
+
+            loss_D = loss_real + loss_fake
+            loss_D.backward()
+            opt_D.step()
+            total_loss_D += loss_D
+
+            # 2.Generator更新
+            G.zero_grad()
+            z2 = torch.randn(real.size(0), z_dim, device=device)
+            fake2 = G(z2)
+            pred2 = D(fake2)
+            loss_G = criterion(pred2, torch.ones_like(pred2))
+            loss_G.backward()
+            opt_G.step()
+            total_loss_G += loss_G
+
+        avg_loss_D = total_loss_D / len(dataloader)
+        avg_loss_G = total_loss_G / len(dataloader)
+
+        if rank == 0:
+            print(f"epoch: {epoch+1}, loss_D: {avg_loss_D:.3f}, loss_G: {avg_loss_G:.3f}")
+
+    # 保存
+    torch.save(G.module.state_dict(), "6_1_gan.pth")
+
+    dist.destroy_process_group()
+
+
+if __name__ == '__main__':
+    world_size = torch.cuda.device_count()
+    print(world_size)
+    spawn(main, args=(world_size,), nprocs=world_size, join=True)
